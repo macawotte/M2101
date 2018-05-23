cf8849fcc0e01044b998246a9821c8216c1718e4
VM IUT <vm@iut>
2018-05-23 11:50:15 +0200

    test 2

--- Pointeurs/sum.c
+++ Pointeurs/sum.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
-double sum1 (double *tab,inttt size){
-
+double sum1 (double *tab, int size){
+	double res = 0;
+	for (int i = 0;i<size;i++){
+		res = res + tab[i];
+	}
+	return res;
 }
 
-double sum2 (){
-
+double sum2 (double *tab, int size){
+	double res = 0;
+	double *tempo = tab;
+	for(double *p = tab;p<tempo;p++){
+		res = res + *p;
+	}
+	return res;
 }
 
 int main (void){
-	
+	int tab[] = {100,10,1,0.1,0.01};
+	printf(sum1(tab,sizeof(tab)));
+	printf(sum2(tab,sizeof(tab)));
 }
